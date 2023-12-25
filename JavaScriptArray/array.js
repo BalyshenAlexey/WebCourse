36a78ae1b4ec7e7306631f1e23d57848f4aef9ae
BalyshenAlexey <balyshen.aleksei@yandex.ru>
2023-12-25 20:20:55 +0700

    Исправлена задача JavaScriptArray

--- JavaScriptArray/array.js
+++ JavaScriptArray/array.js
@@ -1,48 +1,48 @@
 (function () {
-    const array1 = [3, 1, 2, 6, 5, 10, 9, 7, 5, 12];
-    console.log("Массив 1: " + array1.join(", "));
+    const numbersArray = [3, 1, 2, 6, 5, 10, 9, 7, 5, 12];
+    console.log("Массив 1: " + numbersArray.join(", "));
 
-    const arrayAfterSort =
-        array1.sort(function (element1, element2) {
-            return element2 - element1;
-        });
-    console.log("Массив 1 после сортировки: " + arrayAfterSort.join(", "));
-
-    const subarray1 = array1.slice(0, 5);
-    console.log("Подмассив из первых 5 элементов массива 1: " + subarray1.join(", "));
-
-    const subarray2 = array1.slice(array1.length - 5);
-    console.log("Подмассив из последних 5 элементов массива 1: " + subarray2.join(", "));
-
-    const sum = array1.reduce(function (sum, element) {
-        if (element % 2 === 0) {
-            return sum + element;
-        }
-
-        return sum;
-    }, 0);
-
-    console.log("Сумма четных элементов массива 1: " + sum);
-
-    const array2 = [];
-
-    for (let i = 1; i <= 100; i++) {
-        array2.push(i);
+    function sortDescending(array) {
+        return array.sort((element1, element2) => element2 - element1);
     }
 
-    console.log("Массив 2: " + array2.join(", "));
+    console.log("Массив чисел после сортировки: " + sortDescending(numbersArray).join(", "));
 
-    const array3 = array2.map(function (element) {
-        if (element % 2 === 0) {
-            return Math.pow(element, 2);
-        }
-    });
+    function getSubarrayFromFirstFiveNumber(array) {
+        return array.slice(0, 5);
+    }
 
-    array3.sort(function (element1, element2) {
-        return element1 - element2;
-    });
+    console.log("Подмассив из первых 5 элементов массива чисел: " + getSubarrayFromFirstFiveNumber(numbersArray).join(", "));
 
-    array3.splice(50, 50);
+    function getSubarrayFromLastFiveNumber(array) {
+        return array.slice(-5);
+    }
 
-    console.log("Cписок квадратов четных чисел из массива 2: " + array3.join(", "));
-})()
+    console.log("Подмассив из последних 5 элементов массива чисел: " + getSubarrayFromLastFiveNumber(numbersArray).join(", "));
+
+    function getEvenNumbersSum(array) {
+        return array.reduce((sum, number) => {
+            if (number % 2 === 0) {
+                sum += number;
+            }
+
+            return sum;
+        }, 0);
+    }
+
+    console.log("Сумма четных элементов массива 1: " + getEvenNumbersSum(numbersArray));
+
+    const oneUpToOneHundredNumbersArray = [];
+
+    for (let i = 1; i <= 100; i++) {
+        oneUpToOneHundredNumbersArray.push(i);
+    }
+
+    console.log("Массив 2: " + oneUpToOneHundredNumbersArray.join(", "));
+
+    function getEvenNumbersSquaresArray(array) {
+        return array.filter(number => number % 2 === 0).map(number => Math.pow(number, 2));
+    }
+
+    console.log("Список квадратов четных чисел из массива 2: " + getEvenNumbersSquaresArray(oneUpToOneHundredNumbersArray).join(", "));
+})();
