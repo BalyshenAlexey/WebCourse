6e70b3efef51e5f4f928d0b294f5d2c4579948a7
BalyshenAlexey <balyshen.aleksei@yandex.ru>
2024-02-05 20:02:55 +0700

    Исправлена задача TodoListJQuery

--- TodoListJQuery/js/script.js
+++ TodoListJQuery/js/script.js
@@ -6,21 +6,26 @@ $(function () {
     addTodoForm.submit(function (e) {
         e.preventDefault();
 
+        const errorMessage = addTodoForm.find(".error-message");
+
         let newTodoText = newTodoTextField.val().trim();
-        newTodoTextField.removeClass("invalid");
+        newTodoTextField.removeClass("is-invalid");
+        errorMessage.addClass("d-none");
 
         if (newTodoText.length === 0) {
-            newTodoTextField.addClass("invalid");
+            newTodoTextField.addClass("is-invalid");
+            errorMessage.removeClass("d-none");
+
             return;
         }
 
-        const newTodo = $("<li>").addClass("todo-item", "list-group-item");
+        const newTodo = $("<li>").addClass("todo-item row mb-2");
 
         function setViewMode() {
             newTodo.html(`
-                    <span class="todo-item-text"></span>
-                    <div class="buttons-block float-end">
-                        <button class="delete-button btn btn-danger" type="button">Удалить</button>
+                    <span class="todo-item-text col"></span>
+                    <div class="buttons-block col-auto">
+                        <button class="delete-button btn btn-danger me-2" type="button">Удалить</button>
                         <button class="edit-button btn btn-primary" type="button">Редактировать</button>
                     </div>      
             `);
@@ -33,12 +38,12 @@ $(function () {
 
             newTodo.find(".edit-button").click(function () {
                 newTodo.html(`
-                        <input type="text" class="edit-text-field">
-                        <div class="buttons-block float-end">
-                            <button class="save-button btn btn-success" type="button">Сохранить</button>
+                        <input type="text" class="edit-text-field form-text form-control col">
+                        <div class="buttons-block col-auto">
+                            <button class="save-button btn btn-success me-2" type="button">Сохранить</button>
                             <button class="cancel-button btn btn-danger" type="button">Отменить</button>
                         </div>
-                        <div class="error-message">Необходимо указать текст</div>
+                        <div class="error-message d-none text-danger">Необходимо указать текст</div>
                 `);
 
                 const editTextField = newTodo.find(".edit-text-field");
@@ -46,11 +51,15 @@ $(function () {
 
                 function saveChangedTodoText() {
                     const changedTodoText = editTextField.val().trim();
+                    const errorMessage = newTodo.find(".error-message");
 
-                    editTextField.removeClass("invalid");
+                    editTextField.removeClass("is-invalid");
+                    errorMessage.addClass("d-none");
 
                     if (changedTodoText.length === 0) {
-                        editTextField.addClass("invalid");
+                        editTextField.addClass("is-invalid");
+                        errorMessage.removeClass("d-none");
+
                         return;
                     }
 
