cf9d05c3be145d2ddba0db3176ff49356867dfbb
BalyshenAlexey <balyshen.aleksei@yandex.ru>
2024-02-16 19:54:44 +0700

    Исправлена задача Underscore

--- Underscore/js/script.js
+++ Underscore/js/script.js
@@ -4,90 +4,86 @@ Vue.createApp({})
             return {
                 people: [
                     {
+                        id: 1,
                         name: "Алексей",
                         age: 15
                     },
                     {
+                        id: 2,
                         name: "Сергей",
                         age: 25
                     },
                     {
-                        name: "Олег",
-                        age: 30
-                    },
-                    {
-                        name: "Анастасия",
-                        age: 27
-                    },
-                    {
+                        id: 3,
                         name: "Алексей",
                         age: 22
                     },
                     {
+                        id: 4,
+                        name: "Олег",
+                        age: 30
+                    },
+                    {
+                        id: 5,
+                        name: "Анастасия",
+                        age: 27
+                    },
+                    {
+                        id: 6,
                         name: "Леонид",
                         age: 56
                     },
                     {
+                        id: 7,
                         name: "Татьяна",
                         age: 19
                     },
                     {
+                        id: 8,
                         name: "Алексей",
                         age: 20
                     },
                     {
+                        id: 9,
                         name: "Екатерина",
                         age: 31
                     },
                     {
+                        id: 10,
                         name: "Олег",
                         age: 26
                     }
                 ],
-                averageAge: Object,
-                uniqNames: [],
-                twentyToThirtyAgePeople: [],
-                uniqNamesCount: {},
                 isVisible: false
-            }
+            };
         },
 
-        methods: {
-            getAverageAge() {
-                this.averageAge = _.chain(this.people)
-                    .reduce((summaryAge, p) => summaryAge + p.age, 0)
-                    .value() / this.people.length;
+        computed: {
+            averageAge: function () {
+                return _.sumBy(this.people, "age") / _.size(this.people);
             },
 
-            getTwentyToThirtyAgePeople() {
-                this.twentyToThirtyAgePeople = _.chain(this.people)
+            twentyToThirtyAgePeople: function () {
+                return _.chain(this.people)
                     .filter(p => p.age >= 20 && p.age <= 30)
                     .sortBy("age")
                     .value();
             },
 
-            getUniqNames() {
-                this.uniqNames = _.chain(this.people)
+            uniqueNames: function () {
+                return _.chain(this.people)
                     .filter(p => p.age >= 20 && p.age <= 30)
-                    .pluck("name")
+                    .orderBy("name", "desc")
+                    .map("name")
                     .uniq()
-                    .sortBy("name")
                     .value();
             },
 
-            getUniqNamesCount() {
-                this.uniqNamesCount = _.chain(this.people)
+            uniqueNamesCounts() {
+                return _.chain(this.people)
                     .countBy("name")
                     .value();
             },
-
-            getResults() {
-                this.getAverageAge();
-                this.getUniqNames();
-                this.getTwentyToThirtyAgePeople();
-                this.getUniqNamesCount();
-                this.isVisible = true
-            }
         },
 
         template: `
@@ -96,34 +92,42 @@ Vue.createApp({})
 
             <ul class="list-unstyled">
               <li v-for="p in people" class="row">
-                <div class="col-2">{{ p.name }}</div>
+                <div class="col-3">{{ p.name }}</div>
                 <div class="col-auto">{{ p.age }}</div>
               </li>
             </ul>
           </div>
 
-          <button @click="getResults" class="add-button btn btn-success m-2">Получить результат</button>
+          <button @click="isVisible=true" class="add-button btn btn-success m-2">Получить результат</button>
 
-          <div v-if="isVisible" class="m-3">
-            <div class="mb-2">Средний возраст всех людей: {{ this.averageAge }}</div>
+          <div v-if="isVisible" class="mb-5">
+            <div>
+              <div class="mb-2">Средний возраст всех людей: {{ averageAge }}</div>
 
-            <div>Список людей с возрастом от 20 до 30 включительно, отсортированый по возрастанию возраста:
+              <div>
+                Список людей с возрастом от 20 до 30 включительно, отсортированный по возрастанию возраста:
+              </div>
+              <ul class="list-unstyled">
+                <li v-for="p in twentyToThirtyAgePeople"
+                    :key="p.id"
+                    class="row">
+                  <div class="col-3">{{ p.name }}</div>
+                  <div class="col-auto">{{ p.age }}</div>
+                </li>
+              </ul>
+
+              <div>
+                Список уникальных имен людей с возрастом от 20 до 30 включительно, отсортированный по убыванию:
+              </div>
+              <ul class="list-unstyled">
+                <li v-for="p in uniqueNames" :key="p">{{ p }}</li>
+              </ul>
+
+              <div>
+                Объект, в котором ключи - имена людей, а значения – количество людей с этим именем:
+              </div>
+              <div>{{ uniqueNamesCounts }}</div>
             </div>
-            <ul class="list">
-              <li v-for="p in twentyToThirtyAgePeople" class="row">
-                <div class="col-2">{{ p.name }}</div>
-                <div class="col-auto">{{ p.age }}</div>
-              </li>
-            </ul>
-
-            <div>Список уникальных имен людей с возрастом от 20 до 30 включительно, отсортированый по убыванию:
-            </div>
-            <ul class="list-unstyled">
-              <li v-for="name in uniqNames"> {{ name }}</li>
-            </ul>
-
-            <div>Объект, в котором ключи - имена людей, а значения – количество людей с этим именем:</div>
-            <div v-if="isVisible">{{ this.uniqNamesCount }}</div>
-          </div>`
-    })
-    .mount("#app");
+          </div>
+        `
+    }).mount("#app");
